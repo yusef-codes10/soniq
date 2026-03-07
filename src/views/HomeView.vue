8405e50ef5c1a22d013f62d499b974afb0c13272
Yusef Codes <youcefcopy@gmail.com>
2026-03-07 14:00:22 +0100

    cool fonts added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,16 @@
 <script setup></script>
 
 <template>
-  <h1>The is the Home vue</h1>
+  <div class="home">
+    <h1>The is the Home vue</h1>
+    <div class="section-label">Font 1</div>
+    <div class="caption">Font 1</div>
+    <div class="brutalist-title">Font 3</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.home {
+  font-size: 3rem;
+}
+</style>
