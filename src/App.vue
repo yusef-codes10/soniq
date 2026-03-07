b619f04755f652c7e1d121e2d7292e1cb19c162c
Yusef Codes <youcefcopy@gmail.com>
2026-03-07 14:01:35 +0100

    next plans

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,6 @@ import HomeView from './views/HomeView.vue'
 </template>
 
 <style scoped></style>
+
+<!-- TODO: 1- the retro buttons sttyle, the views butons, layouts form ..etc -->
+ <!-- TODO: the home view and small dashboard -->
