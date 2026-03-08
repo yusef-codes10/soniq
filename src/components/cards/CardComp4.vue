8fbb11310da707104daa2ac48e4bdcac5bf548a1
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 12:50:09 +0100

    renaming the cards for the sake of the names

---
+++ src/components/cards/CardComp4.vue
@@ -0,0 +1,65 @@
+<script setup>
+</script>
+<template>
+  <div class="card">
+    <div class="card-inner">
+      <div class="card-front">
+        <p>Front Side</p>
+      </div>
+      <div class="card-back">
+        <p>Back Side</p>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.card {
+  width: 300px;
+  height: 200px;
+  perspective: 1000px;
+}
+
+.card-inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.999s;
+}
+
+.card:hover .card-inner {
+  transform: rotateY(180deg);
+}
+
+.card-front,
+.card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.card-front {
+  background-color: #6a2c70;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  border: 10px solid #6a2c70;
+  border-radius: 10px;
+  justify-content: center;
+  font-size: 24px;
+  transform: rotateY(0deg);
+}
+
+.card-back {
+  background-color: #f08a5d;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  border: 10px solid #f08a5d;
+  border-radius: 10px;
+  justify-content: center;
+  font-size: 24px;
+  transform: rotateY(180deg);
+}
+</style>
