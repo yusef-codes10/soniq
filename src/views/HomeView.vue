bbfaa5af10f70130b994c91837dce66677ee6ae1
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 10:57:57 +0100

    the search input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@
   <div class="home">
     <div class="cta">
       <h1>The Fastest CSS Library Ever</h1>
+      <div class="search-input">
+        <input type="search" name="" id="" />
+      </div>
     </div>
   </div>
 </template>
@@ -12,6 +15,21 @@
 .home {
   font-size: 1rem;
   /* background-color: var(--bg-color); */
-  height: 90dvh;
+  max-height: 90dvh;
+  height: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.search-input {
+  width: 60%;
+}
+
+input {
+  padding: 1rem 2rem;
+  width: 100%;
+  font-size: 2rem;
 }
 </style>
