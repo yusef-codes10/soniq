dadc4430b3d4a5a34ab2647e7a84133b93e3528f
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 10:39:17 +0100

    navbar content, simple navigation links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,28 @@
 <script setup></script>
 
 <template>
-  <nav>navbar</nav>
+  <nav>
+    <h2 class="logo">Soniq</h2>
+    <div class="links">
+      <ul>
+        <li>Layouts</li>
+        <li>buttons</li>
+        <li>forms</li>
+        <li>Components</li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+nav {
+  font-size: 1.5rem;
+  /* background-color: #d7d6cb; */
+  padding: 0.75rem 1rem;
+  border-radius: 0.25rem;
+  border: solid 0.125rem #121212;
+
+  box-shadow: 0.25rem 0.25rem #121212;
+  height: 10dvh;
+}
+</style>
