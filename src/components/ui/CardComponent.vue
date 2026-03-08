ac45b58cba1bdbb3d2171b7865ee894c820c1541
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 12:06:40 +0100

    basic card component

---
+++ src/components/ui/CardComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <div class="card">This is the card</div>
+</template>
+
+<style scoped>
+.card {
+  height: 100px;
+  width: 300px;
+  background-color: springgreen;
+  font-size: 2rem;
+  padding: 0.75rem 1rem;
+  border-radius: 0.25rem;
+  border: solid 0.25rem #121212;
+
+  box-shadow: 0.25rem 0.25rem #121212;
+}
+</style>
