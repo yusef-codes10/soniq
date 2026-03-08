451a4c5bd7694e5b2944308e2643832029bf86c0
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 12:45:46 +0100

    the 2nd card component and data

--- src/data/cards.js
+++ src/data/cards.js
@@ -72,6 +72,75 @@ const cards = [
 
     `,
   },
+  {
+    id: 2,
+    name: 'card-2',
+    html: `
+<div class="card">
+  <div class="card-inner">
+    <div class="card-front">
+      <p>Front Side</p>
+    </div>
+    <div class="card-back">
+      <p>Back Side</p>
+    </div>
+  </div>
+</div>
+    `,
+    css: `
+        .card {
+  width: 300px;
+  height: 200px;
+  perspective: 1000px;
+}
+
+.card-inner {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  transform-style: preserve-3d;
+  transition: transform 0.999s;
+}
+
+.card:hover .card-inner {
+  transform: rotateY(180deg);
+}
+
+.card-front,
+.card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  backface-visibility: hidden;
+}
+
+.card-front {
+  background-color: #6A2C70;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  border: 10px solid #6A2C70;
+  border-radius: 10px;
+  justify-content: center;
+  font-size: 24px;
+  transform: rotateY(0deg);
+}
+
+.card-back {
+  background-color: #F08A5D;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  border: 10px solid #F08A5D;
+  border-radius: 10px;
+  justify-content: center;
+  font-size: 24px;
+  transform: rotateY(180deg);
+}
+
+
+    `,
+  },
 ]
 
 export default cards
