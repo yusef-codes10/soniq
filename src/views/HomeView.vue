1294ab794eca73207449be54991de888127ed698
Yusef Codes <youcefcopy@gmail.com>
2026-03-08 11:00:52 +0100

    better effects for the input

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -28,8 +28,22 @@
 }
 
 input {
-  padding: 1rem 2rem;
+  /* padding: 1rem 2rem; */
   width: 100%;
   font-size: 2rem;
+  padding: 0.75rem 1rem;
+  border-radius: 0.25rem;
+  border: solid 0.25rem #121212;
+
+  box-shadow: 0.25rem 0.25rem #121212;
+
+  outline: none;
+
+  transition: translate 200ms, box-shadow 200ms;
+}
+
+input:hover {
+  translate: 0 0.125rem;
+  box-shadow: 0.125rem 0.125rem #121212;
 }
 </style>
