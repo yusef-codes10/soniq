6bc19f9869922184d08a47cfd4479a3c98f835e6
Yusef Codes <youcefcopy@gmail.com>
2026-03-09 12:21:18 +0100

    accessing the entire vue source code is done | we need the saparate html and css though

--- src/views/ButtonsView.vue
+++ src/views/ButtonsView.vue
@@ -8,6 +8,9 @@ import buttons from '@/data/buttons.js'
     <div class="cards-grid">
       <CardComponent v-for="btn in buttons" :key="btn.id" :title="btn.name">
         <component :is="btn?.component"></component>
+        <pre class="code-block">
+  <code>{{ btn.source }}</code>
+</pre>
       </CardComponent>
       <CardComponent />
       <CardComponent />
