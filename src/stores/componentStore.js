01bd169b40cd381dda5a2581d016587363f0054e
Yusef Codes <youcefcopy@gmail.com>
2026-03-09 14:37:01 +0100

    seting up the pinia store

---
+++ src/stores/componentStore.js
@@ -0,0 +1,6 @@
+import { defineStore } from 'pinia'
+
+export const componentsStore = defineStore('components', () => {
+  // now what we ahve to do is to use and define every sinhle array here
+  // also combine/merge all the arrays here, need them on search
+})
