7756559f8872ae90228d9448bb4b4a54d6a6213f
Yusef Codes <youcefcopy@gmail.com>
2026-03-09 13:41:55 +0100

    CodeSection as saparate component

---
+++ src/components/ui/CodeSection.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { computed } from 'vue'
+import parseVueSource from '@/utils/parseVueSource.js'
+
+const props = defineProps({
+  source: String,
+})
+
+const parsed = computed(() => parseVueSource(props.source))
+</script>
+
+<template>
+  <div class="code-container">
+    <div class="code-header">HTML</div>
+    <pre class="code">{{ parsed.template }}</pre>
+
+    <div class="code-header">CSS</div>
+    <pre class="code">{{ parsed.styles }}</pre>
+  </div>
+</template>
