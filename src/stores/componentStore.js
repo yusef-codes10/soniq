925afde0d541346e12448700542296c14b835288
Yusef Codes <youcefcopy@gmail.com>
2026-03-09 14:39:50 +0100

    the array have been defined in the piia store

--- src/stores/componentStore.js
+++ src/stores/componentStore.js
@@ -1,6 +1,17 @@
 import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import buttons from '@/data/buttons.js'
+import cards from '@/data/cards.js'
 
 export const componentsStore = defineStore('components', () => {
   // now what we ahve to do is to use and define every sinhle array here
   // also combine/merge all the arrays here, need them on search
+  //  * state
+  const buttonsArray = ref(buttons)
+  const cardsArray = ref(cards)
+
+  return {
+    buttonsArray,
+    cards,
+  }
 })
