1f7e75db46e04c9556bd37c083b0e5768e06e221
Yusef Codes <youcefcopy@gmail.com>
2026-03-09 13:48:13 +0100

    accessing the source attribute in the codeComponent that recieves from the different views

--- src/views/ButtonsView.vue
+++ src/views/ButtonsView.vue
@@ -1,29 +1,20 @@
 <script setup>
 import CardComponent from '@/components/ui/CardComponent.vue'
+import CodeSection from '@/components/ui/CodeSection.vue'
 import buttons from '@/data/buttons.js'
-import parseVueSource from '@/utils/parseVueSource.js'
 </script>
 
 <template>
   <div class="buttons">
     <div class="cards-grid">
       <CardComponent v-for="btn in buttons" :key="btn.id" :title="btn.name">
-        <component :is="btn?.component"></component>
-        <div class="code-container">
-          <div class="code-header">HTML</div>
+        <!-- preview -->
+        <component :is="btn.component" />
 
-          <pre class="code"
-            >{{ parseVueSource(btn.source).template }}
-  </pre
-          >
-
-          <div class="code-header">CSS</div>
-
-          <pre class="code"
-            >{{ parseVueSource(btn.source).styles }}
-  </pre
-          >
-        </div>
+        <!-- code -->
+        <template #code>
+          <CodeSection :source="btn.source" />
+        </template>
       </CardComponent>
       <CardComponent />
       <CardComponent />
