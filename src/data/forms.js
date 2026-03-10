bd16dae3bca9394200bd87837611666592bd3a99
Yusef Codes <youcefcopy@gmail.com>
2026-03-10 12:18:05 +0100

    adding some forms, retro form

---
+++ src/data/forms.js
@@ -0,0 +1,16 @@
+import ButtonComp1 from '@/components/buttons/ButtonComp1.vue'
+import ButtonComp1Source from '@/components/buttons/ButtonComp1.vue?raw'
+import { markRaw } from 'vue'
+
+// buttons array
+const buttons = [
+  {
+    id: 1,
+    name: 'Retro Button',
+    slug: 'retro-button',
+    component: markRaw(ButtonComp1),
+    source: ButtonComp1Source,
+  },
+]
+
+export default buttons
--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -1,13 +1,26 @@
 <script setup>
+import CardComponent from '@/components/ui/CardComponent.vue'
+// import CodeSection from '@/components/ui/CodeSection.vue'
+
+import { componentsStore } from '@/stores/componentStore.js'
+
+const myStore = componentsStore()
 </script>
 
 <template>
-  <div class="forms">This is the forms view</div>
+  <div class="forms">
+    <div class="cards-grid">
+      <CardComponent v-for="btn in myStore.buttonsArray" :key="btn.id" :comp="btn">
+        <!-- preview -->
+        <component :is="btn.component" />
+      </CardComponent>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .forms {
   height: 90dvh;
-  background-color: sienna;
+  /* background-color: sienna; */
 }
 </style>
