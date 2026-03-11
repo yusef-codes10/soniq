0412a7c667bbf7d9fc6f7cfa6a69c6fb80a9fe08
Yusef Codes <youcefcopy@gmail.com>
2026-03-11 16:08:23 +0100

    rendering the navbar to the layouts view, still have to render it to the details page

--- src/views/LayoutsView.vue
+++ src/views/LayoutsView.vue
@@ -1,8 +1,25 @@
 <script setup>
+import CardComponent from '@/components/ui/CardComponent.vue'
+
+import { componentsStore } from '@/stores/componentStore.js'
+
+const myStore = componentsStore()
 </script>
 
 <template>
-  <div class="layouts">This is the layouts view</div>
+  <div class="layouts">
+    <div class="cards-grid">
+      <CardComponent v-for="layout in myStore.layoutsArray" :key="layout.id" :comp="layout">
+        <!-- preview -->
+        <component :is="layout.component" />
+
+        <!-- code -->
+        <!-- <template #code>
+          <CodeSection :source="btn.source" />
+        </template> -->
+      </CardComponent>
+    </div>
+  </div>
 </template>
 
 <style scoped>
