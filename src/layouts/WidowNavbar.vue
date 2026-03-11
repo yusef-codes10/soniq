a8f67e465a51b223c11f3443a0367aaf22368168
Yusef Codes <youcefcopy@gmail.com>
2026-03-11 15:58:58 +0100

    the widow navbar

--- src/layouts/WidowNavbar.vue
+++ src/layouts/WidowNavbar.vue
@@ -2,8 +2,28 @@
 </script>
 
 <template>
-  <div></div>
+  <nav>
+    <div class="logo">
+      <h1>Widow</h1>
+    </div>
+    <div class="links">links</div>
+    <div class="bt"></div>
+  </nav>
 </template>
 
 <style scoped>
+:root {
+  --brand-color: #dd3c27;
+  --brand-hover: #ef4646;
+  --brand-transition: #150807;
+
+  --bg-color: #080808;
+  --bg-secondary: #111111;
+
+  --font: 'Rajdhani, Orbitron, sans-serif';
+}
+
+nav {
+  font-family: var(--font);
+}
 </style>
